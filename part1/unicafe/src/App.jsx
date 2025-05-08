193289a0451dea875ba1f973c3290990971a3fe1
cpcap <hi1214justin@gmail.com>
2025-05-08 11:57:37 +0800

    Finish 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,20 +14,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(good + neutral + bad);
+  const [average, setAverage] = useState(0);
+
+  const calculateAverage = () => all ? (good * 1 + bad * -1) / (good + neutral + bad) : 0
+  const calculatePositive = () => all ? good / all : 0
+
+  const addGood = () => {
+    setGood(good + 1)
+    const updatedGood = good + 1
+    setAll(updatedGood + neutral + bad)
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setAll(good + updatedNeutral + bad)
+  }
+  const addBad = () => {
+    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setAll(good + neutral + updatedBad)
+  }
+
 
   return (
     <div>
       <h1>give feedback</h1>
       <div style={{display: 'flex'}}>
-        <RatingButton text={"good"} onClick={() => setGood(good + 1)}></RatingButton>
-        <RatingButton text={"neutral"} onClick={() => setNeutral(neutral + 1)}></RatingButton>
-        <RatingButton text={"bad"} onClick={() => setBad(bad + 1)}></RatingButton>
+        <RatingButton text={"good"} onClick={addGood}></RatingButton>
+        <RatingButton text={"neutral"} onClick={addNeutral}></RatingButton>
+        <RatingButton text={"bad"} onClick={addBad}></RatingButton>
       </div>
       <h1>statistics</h1>
       <div>
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+      </div>
+      <div>
+        <div>all {all}</div>
+        <div>average {calculateAverage()}</div>
+        <div>positive {calculatePositive()} %</div>
       </div>
     </div>
   )
