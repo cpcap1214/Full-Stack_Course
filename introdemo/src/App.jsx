03931921929b988d3de202b64b1e69b284aa4c85
Justin Chung <hi1214justin@gmail.com>
2025-07-15 00:41:23 +0800

    Finish 2.6

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react"
 import Note from "./conponents/Note"
 
-const App = ({ notes }) => {
-
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('new note')
+  const [showAll, setShowall] = useState(true)
+  const noteToShow = showAll ? notes : notes.filter((note) => note.important === true);
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('Button clicked', event.target)
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowall(!showAll)}>Toggle show all</button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {noteToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  const handleAddPerson = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleAddPerson}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
