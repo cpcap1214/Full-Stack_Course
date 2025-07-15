971c760874a77ff3d987acaa2220dcd8956cfba6
Justin Chung <hi1214justin@gmail.com>
2025-07-16 00:05:16 +0800

    Finish 2.11

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import Note from "./conponents/Note"
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('new note')
   const [showAll, setShowall] = useState(true)
   const noteToShow = showAll ? notes : notes.filter((note) => note.important === true);
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  },[])
+
   const addNote = (event) => {
     event.preventDefault()
     console.log('Button clicked', event.target)
