b90084b7c1132179754d2fe012d303bd39dc10d7
Justin Chung <hi1214justin@gmail.com>
2025-07-28 23:54:24 +0800

    Finish 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const count = persons.length
+    const now = new Date()
+    response.send(`Phonebook has info of ${count} people<br>${now}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
